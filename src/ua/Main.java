61012b638587a13fbf943923a2c2bfc66b142433
GorbunovRoman <gorbunovr@edi-n.com>
2020-07-20 13:25:47 +0300

    AdKernel test task

---
+++ src/ua/Main.java
@@ -0,0 +1,26 @@
+package ua;
+
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        new Main().main();
+    }
+
+    public void main()
+    {
+        OAHashTable<Integer, Long> map = new OAHashTable<>(200);
+
+        map.put(300, 150L);
+        map.put(301, 175L);
+        map.put(500, 200L);
+        map.put(301, null);
+        map.put(302, 175L);
+
+        assert map.get(300) == 150L;
+        assert map.get(301) == null;
+        assert map.get(302) == 175L;
+        assert map.get(500) == 200L;
+    }
+}
---
+++ src/ua/OAHashTable.java
@@ -0,0 +1,64 @@
+package ua;
+
+class OAHashTable<T1, T2> {
+    private Pair<T1, T2>[] table;
+    private int size = 0;
+
+    public OAHashTable(int size) {
+        table = new Pair[size];
+    }
+
+    public boolean put(T1 key, T2 value)
+    {
+        int pos = key.hashCode() % table.length;
+
+        if(size++ != table.length) {
+            for (int i = pos; i < table.length; ++i)
+                if(putAtPos(i, key, value)) return true;
+
+            for (int i = 0; i < pos; ++i)
+                if(putAtPos(i, key, value)) return true;
+        }
+
+        return false;
+    }
+
+    public boolean putAtPos(int pos, T1 key, T2 value)
+    {
+        Pair posValue = table[pos];
+
+        if (posValue == null) {
+            table[pos] = new Pair(key, value);
+            return true;
+        } else if (posValue.getKey().equals(key)) {
+            table[pos].setValue(value);
+            return true;
+        }
+
+        return false;
+    }
+
+    public T2 get(T1 key)
+    {
+        int pos = key.hashCode() % table.length;
+
+        for (int i = pos; i < table.length; ++i)
+        {
+            Pair<T1, T2> value = table[i];
+            if(value != null && value.getKey().equals(key)) return value.getValue();
+        }
+
+        for (int i = 0; i < pos; ++i)
+        {
+            Pair<T1, T2> value = table[i];
+            if(value != null && value.getKey().equals(key)) return value.getValue();
+        }
+
+        return null;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+}
